a9c361f6a1ce3075b1c20c8052fbbb8cc1ac9b82
Miguel París Díaz <mparisdiaz@gmail.com>
2012-07-11 17:25:18 +0200

    Add method to assert media session is not null in TestCaseBase.

--- src/test/java/com/kurento/mscontrol/commons/junit/util/TestCaseBase.java
+++ src/test/java/com/kurento/mscontrol/commons/junit/util/TestCaseBase.java
@@ -1,10 +1,17 @@
 package com.kurento.mscontrol.commons.junit.util;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.kurento.mscontrol.commons.MediaSession;
 
 public abstract class TestCaseBase extends TestCase {
+
+	protected static final Logger log = LoggerFactory
+			.getLogger("KcMsControlTests");
 
 	private static MediaSession mediaSession = null;
 
@@ -14,6 +21,12 @@ public abstract class TestCaseBase extends TestCase {
 
 	public static void setMediaSession(MediaSession mediaSession) {
 		TestCaseBase.mediaSession = mediaSession;
+	}
+
+	protected void checkMediaSessionIsNotNull(MediaSession mediaSession) {
+		Assert.assertNotNull(
+				"mediaSession is null, it must be assigned using setMediaSession() static method",
+				mediaSession);
 	}
 
 }
